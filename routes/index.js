e158e2fb7b723fc7c6c95f96f6ed19b4314a6231
Vincent Markham <vincent.markham@gmail.com>
2016-04-20 15:16:32 -0700

    Make master function

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Friendface' });
 });
 
-<<<<<<< HEAD
-router.get('/', function(req, res, next) {
-  res.render('register');
-=======
-// profile route for Kay's testing purposes
-router.get('/profile', function(req, res, next) {
-  res.render('profile');
-});
 
-// edit route for Kay's testing purposes
-router.get('/edit', function(req, res, next) {
-  res.render('edit');
->>>>>>> 176f246f239d2e336dc8ea6d97cb8cc79b96c072
-});
+
 
 module.exports = router;
