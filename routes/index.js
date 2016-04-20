d09b9dd01b37484b6af9348163e1fd710fcff399
Kay McKelly <frecklestars@gmail.com>
2016-04-20 10:45:03 -0700

    Profile and edit page updates

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Friendface' });
 });
 
+// register route for Kay's testing purposes
 router.get('/register', function(req, res, next) {
   res.render('register');
+});
+
+// profile route for Kay's testing purposes
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+// edit route for Kay's testing purposes
+router.get('/edit', function(req, res, next) {
+  res.render('edit');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('users', { title: 'Friendface Users' });
 });
 
-// from Kay - I changed this to reflect the naming structure of the register.jade page I created for the sign up form
+// note from Kay - I changed this to reflect the naming structure of the register.jade page I created for the sign up form
 router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
-// from Kay - still needs to be changed to correct path
+// note from Kay - still needs to be changed to correct path
 router.post('/signup',
   passport.authenticate('local-signup', {
     successRedirect: '/profile',
